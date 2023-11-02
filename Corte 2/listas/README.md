50f0be186b5fd796b33c3bf6a4f950709acad31f
Julian Javier Lizcano Villarreal <jjlizcano6@gmail.com>
2023-11-02 12:09:52 -0500

    organización de carpetas y archivos

---
+++ Corte 2/listas/README.md
@@ -0,0 +1,21 @@
+# lista
+
+Crear una lista sin usar métodos de python con opciones de insertar al inicio, recorrer lista, insertar por valor, eliminar por valor, eliminar por posición, buscar por valor, etc.
+
+# x_palabra_x
+
+A partir de una frase, si el número de sus palabras es superior a 9, convertir a X las palabras ubicadas en las posiciones pares y volver a mostrar la nueva frase.
+
+HOLA MUY BUENOS DÍAS LISTOS PARA TRABAJAR HOY VIERNES TARDE
+
+X MUY X DIAS X PARA X TRABAJAR X SABADO X
+
+# factura_simple
+
+Una empresa tiene para la venta 5 productos diferentes con sus precios, elaborar
+un programa que le muestre al usuario el menú de productos (código,
+nombre valor unitario), y permita facturar una venta (validar que máximo se
+puedan seleccionar 5 productos para la venta), se digita el código de un producto
+y La cantidad a comprar, se debe visualizar el total de la compra teniendo en
+cuenta agregar el 19% de Iva. El programa debe validar que la digitación del
+código del producto sea correcta, es decir se encuentre en la lista.
--- Corte 2/listas/lista.py
+++ Corte 2/listas/lista.py
@@ -1,38 +1,78 @@
-class Nodo:
-    def __init__(self, valor):
-        self.valor = valor
-        self.sig = None
-        self.ant = None
+import os
 
-def insertar_inicio(self, valor):
-    if self.primer_nodo is None:
-        self.insertar_vacia(valor)
-        return
-    else:
+
+class Lista:
+    def __init__(self):
+        Lista.primer_nodo = None
+
+    def insertar_inicio(self, valor):
         nuevo_nodo = Nodo(valor)
-        nuevo_nodo.sig = self.primer_nodo
-        nuevo_nodo.ant = nuevo_nodo
+        nuevo_nodo.dir = self.primer_nodo
         self.primer_nodo = nuevo_nodo
 
-def insertar_final(self, valor):
-    if self.primer_nodo is None:
-        self.insertar_vacia(valor)
-        return
-    
-def recorrer_lista(self):
-    if self.primer_nodo is None:
-        print("No existe lista a recorrer")
-        return
-    else:
-        n = self.primer_nodo
-        while n is not None:
-            print(n.valor)
-            n = n.sig
-    
+    def recorrer_lista(self):
+        if self.primer_nodo is None:
+            print("No existe lista a recorrer")
+        else:
+            nodo = self.primer_nodo
+            while nodo is not None:
+                print(nodo.item)
+                nodo = nodo.dir
 
-    n = self.primer_nodo
-    while n.sig is not None:
-        n = n.sig
-    nuevo_nodo = Nodo(valor)
-    n.sig = nuevo_nodo
-    nuevo_nodo.ant = n
+    def agregar_nodo_y_desplazar(self, nodo, nuevo_nodo):
+        nuevo_nodo.dir = nodo.dir
+        nodo.dir = nuevo_nodo # FOTY
+
+    def insertar_por_valor(self, valor, nuevo_valor):
+        nuevo_nodo = Nodo(nuevo_valor)
+        if self.primer_nodo is None:
+            print("No existe lista a recorrer")
+        else:
+            nodo = self.primer_nodo
+
+            while nodo is not None:
+                nodo = nodo.dir
+
+                if nodo.item == valor:
+                    self.agregar_nodo_y_desplazar(nodo, nuevo_nodo)
+                    break
+                    
+
+
+
+class Nodo:
+    def __init__(self, valor):
+        self.item = valor
+        self.dir = None
+
+lista = Lista()
+
+while True:
+    print("_______________________")
+    print("|        Menu         |")
+    print("|1. Insertar al inicio|")
+    print("|2. Recorrer lista    |")
+    print("|3. Insertar por valor|")
+    print("|4. Salir             |")
+    print("-----------------------")
+
+    opcion = int(input("Ingrese una opcion: "))
+    if opcion == 1:
+        os.system("cls")
+        valor = int(input("Ingrese un valor: "))
+        lista.insertar_inicio(valor)
+    elif opcion == 2:
+        print("La lista es:")
+        lista.recorrer_lista()
+        print()
+    
+    elif opcion == 3:
+        valor = int(input("Ingrese el valor a remplazar: "))
+        nuevo_valor = int(input("Ingrese un nuevo valor: "))
+        lista.insertar_por_valor(valor, nuevo_valor)
+    elif opcion == 4:
+        break
+    else:
+        print("Opcion incorrecta")
+        # limpiar pantalla
+        os.system("cls")
